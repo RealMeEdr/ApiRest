a8a51d90f787afce8849dcdcf64611fb1855f56b
RealMeEdr <edreisegundo@gmail.com>
2021-08-13 22:19:33 -0500

    Actualizacion de API

--- APIREST/Program.cs
+++ APIREST/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.Net;
 using System.Net.Http;
@@ -14,13 +15,14 @@ namespace APIREST
     class Program
     {
         
-        private static async Task ObtenerToken()
+        public static async Task ObtenerToken()
         {
             //Por favor poner un interruptor en la linea 38 para mostrar el resultado 
             try
             {
 
                 var urlToken = "https://api.apoloservice.com/api/Sesion/RevisaSesion";
+                var urlKeyword = "https://api.apoloservice.com/api/Alertas/NewListadoKeywords";
                 var cliente = new HttpClient();
 
                 Persona post = new Persona()
@@ -28,18 +30,53 @@ namespace APIREST
                     Correo = "prueba0704758@osiris.com",
                     Password = "123456"
                 };
+                Keyword edr = new Keyword()
+                {
+                    KeyString = "Edrei Prueba"
+                };
 
-                var data = JsonSerializer.Serialize<Persona>(post);
+                //serializacion de Objeto Persona
+                var data = JsonConvert.SerializeObject(post);
                 HttpContent contenido =
                     new StringContent(data, System.Text.Encoding.UTF8, "application/json");
 
+                //serializacion de objeto Keyword
+                var datos = JsonConvert.SerializeObject(edr);
+                HttpContent content =
+                    new StringContent(data, System.Text.Encoding.UTF8, "application/json");
+
+                //respuestas HTTP
+                
                 var httpResponse = await cliente.PostAsync(urlToken, contenido);
+                var httpResponseKeywords = await cliente.PostAsync(urlKeyword, contenido);
+                var httpAgreagrKey = await cliente.PostAsync(urlKeyword, content);
                 if (httpResponse.IsSuccessStatusCode)
                 {
+                    var resultKey = await httpResponseKeywords.Content.ReadAsStringAsync();
                     var result = await httpResponse.Content.ReadAsStringAsync();
+                    var resultAgregarKey = await httpAgreagrKey.Content.ReadAsStringAsync();
+                    
+                    //Desearilzacion de Post
+                    var postResult = JsonConvert.DeserializeObject(result);
+                    var postKey = JsonConvert.DeserializeObject(resultKey);
 
-                    var postResult = JsonSerializer.Deserialize<Persona>(result);
+                    //Desearializacion de edr
+                    var postAgregarKey = JsonConvert.DeserializeObject(resultAgregarKey);
+                    
+                    
+                    //se imprime
+                    Console.WriteLine(postResult.ToString());
+                    Console.WriteLine();
+                    Console.WriteLine("Aqui se obtiene el KeyWords");
+                    Console.WriteLine(postKey.ToString());
+                    Console.WriteLine();
+                    Console.WriteLine("Aqui se agrega el Key");
+                    Console.WriteLine(postAgregarKey.ToString());
+
                 }
+                Console.WriteLine("termina aqui");
+                
+
 
             } catch (Exception ex)
             {
@@ -48,25 +85,21 @@ namespace APIREST
         }
         private static async Task ObtenerKeyword()
         {
-            //Por favor poner un interruptor en la linea 77 para mostrar el resultado 
+            //Por favor poner un interruptor en la linea 77 para mostrar el resultado
             try
             {
                 var urlKeyword = "https://api.apoloservice.com/api/Alertas/NewListadoKeywords";
                 var cliente = new HttpClient();
-
-                Persona post = new Persona()
+                Persona token = new Persona()
                 {
-                    Id= 0,
+                    Id = 0,
                     PersonaID = 5611,
-                    Contacto= "Osiris Prueba nuevo home 2021",
-                    Correo= "prueba0704758@osiris.com",
-                    Password = "123456",
-                    Token= "rzDoTZiZ9-2gXNfr27Vamlu6G3UP20GTExY_YkoehnCFE67E-yZF4mr4HFSaei7tvWAjHntLOIkoWg4ltKQ2CXa0PYYRSWCetiDZCKBIkOAIpok79CtgDgfqcGwySIGVvzL5UxWJ_LA__jwSKzqYtg..",
-                    TipoSesion= 1,
-                    TokenSubscription = "null"
+                    Contacto = "Osiris Prueba nuevo home 2021",
+                    Correo = "prueba0704758@osiris.com",
+                    Password = null,
+                    Token = "rzDoTZiZ9-2gXNfr27Vamlu6G3UP20GTExY_YkoehnCFE67E-yZF4mr4HFSaei7tvWAjHntLOIkoWg4ltKQ2CXa0PYYRSWCetiDZCKBIkOAIpok79CtgDgfqcGwySIGVvzL5UxWJ_LA__jwSKzqYtg.."
                 };
-
-                var data = JsonSerializer.Serialize<Persona>(post);
+                var data = JsonConvert.SerializeObject(token);
                 HttpContent contenido =
                     new StringContent(data, System.Text.Encoding.UTF8, "application/json");
 
@@ -74,8 +107,8 @@ namespace APIREST
                 if (httpResponse.IsSuccessStatusCode)
                 {
                     var result = await httpResponse.Content.ReadAsStringAsync();
-
-                    var postResult = JsonSerializer.Deserialize<Persona>(result);
+                    var postResult = JsonConvert.DeserializeObject(result);
+                    Console.WriteLine(postResult.ToString());
                 }
 
             }
@@ -87,27 +120,7 @@ namespace APIREST
         static async Task Main(string[] args)
         {
 
-            int opcion;
-            Console.WriteLine("Que deseas?");
-            Console.WriteLine("1- Obtener Token / " +
-                "2- Obtener Keyword / " +
-                "3- Agregar Keyword / ");
-
-            opcion = Convert.ToInt32(Console.ReadLine());
-
-            switch (opcion)
-            {
-                case 1:
-                    await ObtenerToken();
-                    break;
-                case 2:
-                    await ObtenerKeyword();
-                    break;
-                case 3:
-                    break;
-                default:
-                    break;
-            }
+            await ObtenerToken();
         }
     }
 }
